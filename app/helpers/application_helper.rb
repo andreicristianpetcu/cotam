2187c8c35393d1e3ee41a5f2b3244f8bf59d67c4
Andrei Cristian Petcu <andrei@ceata.org>
2014-03-15 15:46:50 +0100

    first parser, ugly stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
+require 'nokogiri'
+
 module ApplicationHelper
+  def self.generate_events(event_data_source)
+    page = download_page(event_data_source)
+    #download page
+    #parse page
+    #check if each event is duplicate
+    #return onlyuniqueevents
+  end
+
+  def self.download_page(event_data_source)
+    page_url = event_data_source.url
+    page = Nokogiri::HTML(open(page_url))
+    selector = 'div.newsline_item'
+    page.css(selector).each do |el|
+         puts el.text
+    end
+  end
 end
