c8471efbc023b780afe0e1a2a903b442eac09493
Andrei Cristian Petcu <andrei@ceata.org>
2014-03-15 12:31:59 +0100

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cotam::Application.config.secret_key_base = '479766a3f0a631cb8cf56dc8da30ef33439ccc5bbe2a4fda475c1ea6cca2ea12c0866680ef7950bac4d666f283edfc0c8724eefb7867d96002c4df0c8a275fe8'
