13de0eb28368cd3a3aeaaa186c4814dfa540e5b6
Andrei Cristian Petcu <andrei@ceata.org>
2014-03-16 14:16:10 +0100

    home path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Cotam::Application.routes.draw do
+  root "event#index"
   get "event/index"
+  # resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
