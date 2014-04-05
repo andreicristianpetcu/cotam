8897c82ad6378c1593cc39920a5921a663c4b5c2
Andrei Cristian Petcu <andrei@ceata.org>
2014-04-05 13:22:40 +0300

    add new events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,16 @@ require 'nokogiri'
 module ApplicationHelper
   def self.generate_events(event_data_source)
     if event_data_source.nil?
-      event_data_source = EventDataSource.first
+      EventDataSource.find_each do |eds|
+        download_page(eds)
+      end
     end
     #check if each event is duplicate
     #return onlyuniqueevents
   end
 
   def self.download_page(event_data_source)
+    puts "Checking EDS " + event_data_source.to_s
     page_url = event_data_source.url
     page = Nokogiri::HTML(open(page_url))
     item_selector = event_data_source.item_selector
@@ -20,7 +23,8 @@ module ApplicationHelper
       e.url = item.css(name_selector)[0]["href"]
       old = Event.find_by_url(e.url)
       if old == nil then
-        e.save
+        puts "saved " + e.to_yaml
+        e.save!
       end
     end
   end
